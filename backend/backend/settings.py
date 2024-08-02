7da37d2513ce17db41bf8bb05d607c05f14ffc7e
Evgenii Stoliarov <stoliarovea@gmail.com>
2024-08-02 19:57:31 +0300

    02.08.2024-4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['foodgram-yandex.zapto.org']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
